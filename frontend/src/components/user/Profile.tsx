72c909da576016c5f476e3ab5b4f86c9c5be7b63
Markku Rikola <markku.rikola@gmail.com>
2024-01-02 23:38:19 +0200

    fix logout button

--- frontend/src/components/user/Profile.tsx
+++ frontend/src/components/user/Profile.tsx
@@ -1,17 +1,20 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { get } from "../../services/ApiService";
 import { UserInfoContext } from "../provider/UserInfoProvider";
 
 const Profile = () => {
   const user = useContext(UserInfoContext);
+  const navigate = useNavigate();
   if (user) {
     return (
       <>
         this is you: {user.firstName} {user.lastName}
         <div>click these and watch the console</div>
         <div>
-          <button type="button" className="btn btn-primary" 
+          <button
+            type="button"
+            className="btn btn-primary"
             onClick={() =>
               get("/user/profile").then(async (resp) =>
                 console.log(await resp.text())
@@ -20,10 +23,22 @@ const Profile = () => {
           >
             do normal user shit
           </button>
-          <button type="button" className="btn btn-secondary" onClick={() => get("/admin")}>do admin shit</button>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => get("/admin")}
+          >
+            do admin shit
+          </button>
         </div>
         <div>
-          <button type="button" className="btn btn-danger" onClick={() => get("/logout")}>log me out</button>
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={() => navigate("/logout")}
+          >
+            log me out
+          </button>
           <Link style={{ color: "red" }} to={"/logout"}>
             log me out
           </Link>
