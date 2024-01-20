9c00d386c0b54f9697870efbcf9e1862b577bc6c
Markku Rikola <markku.rikola@gmail.com>
2024-01-20 15:50:15 +0200

    Give rating a default value instead

--- backend/src/migration/1705758483083-RatingDefault.ts
+++ backend/src/migration/1705758483083-RatingDefault.ts
@@ -1,16 +1,23 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class RatingDefault1705758483083 implements MigrationInterface {
-    name = 'RatingDefault1705758483083'
+  name = "RatingDefault1705758483083";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "rating" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "rating" SET DEFAULT '1600'`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" ALTER COLUMN "rating" SET DEFAULT '1600'`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" ALTER COLUMN "rating" SET NOT NULL`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "rating" DROP DEFAULT`);
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "rating" DROP NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" ALTER COLUMN "rating" DROP DEFAULT`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" ALTER COLUMN "rating" DROP NOT NULL`
+    );
+  }
 }
