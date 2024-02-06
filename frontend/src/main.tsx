26b9a094163a2881e041af3b051067bdfb8018ab
Markku Rikola <markku.rikola@gmail.com>
2024-02-06 22:55:08 +0200

    remove react.strict

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
@@ -8,19 +7,17 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import "./scss/style.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Motticon</title>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
-    </HelmetProvider>
+  <HelmetProvider>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Motticon</title>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
     <App />
-  </React.StrictMode>
+  </HelmetProvider>
 );
