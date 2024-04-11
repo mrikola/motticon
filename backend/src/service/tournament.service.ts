34f99637714a3e863bdea81255e18906688b542f
Markku Rikola <markku.rikola@gmail.com>
2024-04-11 19:20:40 +0300

    Move simulation back to tournament service, show best result

--- backend/src/service/tournament.service.ts
+++ backend/src/service/tournament.service.ts
@@ -5,7 +5,6 @@ import { partition, round } from "lodash";
 import {
   DraftPodGenerationStrategy,
   PreferencesByPlayer,
-  TournamentsByType,
 } from "../dto/tournaments.dto";
 import { Round } from "../entity/Round";
 import { Draft } from "../entity/Draft";
@@ -24,6 +23,18 @@ import { PreferenceService } from "./preference.service";
 import { randomize } from "../util/random";
 import { makeArray } from "../util/array";
 import { Preference } from "../entity/Preference";
+import { playerToDto } from "../dto/user.dto";
+
+type PreferentialPodAssignments = {
+  preferencePoints: number;
+  assignments: {
+    draftNumber: number;
+    pods: {
+      cube: Cube;
+      players: User[];
+    }[];
+  }[];
+};
 
 export class TournamentService {
   private appDataSource: DataSource;
@@ -335,198 +346,9 @@ export class TournamentService {
         })
       );
     } else {
-      console.log("generating pods based on preferences, watch out");
-      const assignments: User[][][] = makeArray(
-        tournament.drafts.length,
-        podsPerDraft,
-        8
+      console.log(
+        "TODO: generate preferential pod assignments, pick the best one and use it"
       );
-
-      const preferences =
-        await this.preferenceService.getPreferencesForTournament(tournamentId);
-
-      const podGenerationStrategies: DraftPodGenerationStrategy[] = [
-        "greedy",
-        "sparing",
-        "sparing",
-      ];
-
-      const preferencesByPlayer: PreferencesByPlayer = {};
-      preferences.forEach((preference) => {
-        let currentPlayerPreference = preferencesByPlayer[preference.player.id];
-        const pref = {
-          player: preference.playerId,
-          cube: preference.cube.id,
-          points: preference.points,
-          used: false,
-        };
-
-        if (!currentPlayerPreference) {
-          preferencesByPlayer[preference.player.id] = [pref];
-        } else {
-          currentPlayerPreference.push(pref);
-        }
-      });
-
-      const wildCardAssignments: {
-        [key: number]: number[];
-      } = {};
-
-      let draftIndex = 0;
-      for (let draft of tournament.drafts.sort(
-        (a, b) => a.draftNumber - b.draftNumber
-      )) {
-        let preferencePointsUsed = 0;
-        let unassignedPlayers = enrollments.map((enroll) => enroll.player);
-        const draftPods: DraftPod[] = [];
-
-        const wildCards = unassignedPlayers
-          .filter(
-            (player) =>
-              !preferencesByPlayer[player.id] ||
-              preferencesByPlayer[player.id].filter((pref) => !pref.used)
-                .length === 0
-          )
-          .sort(randomize);
-
-        for (let podNumber = 1; podNumber <= podsPerDraft; ++podNumber) {
-          const cubeIndex =
-            podGenerationStrategies[draftIndex] === "greedy"
-              ? 0 // for greedy strategy, take the most popular cube available
-              : podsPerDraft - podNumber; // for sparing strategy, take the Nth most popular where N is pods to be generated
-
-          const cubesByPreference = cubes
-            // filter out cubes already used in this draft
-            .filter((cube) => !draftPods.find((pod) => pod.cube.id === cube.id))
-            .map((cube) => ({
-              id: cube.id,
-              points: preferences
-                .filter(
-                  (pref) =>
-                    pref.cube.id === cube.id &&
-                    !preferencesByPlayer[pref.player.id].find(
-                      (x) => x.cube === cube.id
-                    )?.used
-                )
-                .reduce((acc, cur) => acc + cur.points, 0),
-            }))
-            .sort((a, b) => b.points - a.points);
-
-          const currentCubeId = cubesByPreference[cubeIndex].id;
-          const preferredPlayers = preferences // find players who..
-            .filter((pref) => pref.cube.id === currentCubeId) // want to play this cube
-            .sort((a, b) => b.points - a.points) // have rated it highly
-            .filter(
-              // have not already been assigned to play it in an earlier draft
-              (pref) =>
-                !preferencesByPlayer[pref.player.id].find(
-                  (x) => x.cube === currentCubeId
-                )?.used
-            )
-            .filter(
-              (
-                pref // and have not been assigned to a different cube in this draft
-              ) =>
-                unassignedPlayers.find((player) => player.id === pref.player.id)
-            )
-            .slice(0, 8)
-            .map((pref) => {
-              preferencePointsUsed += pref.points;
-              return pref;
-            })
-            .map((pref) => pref.player);
-
-          // if pod is not full, fill it with wildcards
-          while (preferredPlayers.length < 8 && wildCards.length > 0) {
-            // TODO check that the wildcard hasn't played this cube earlier
-            const assigned = wildCards
-              .filter(
-                (wc) =>
-                  !(wildCardAssignments[wc.id] ?? []).includes(currentCubeId)
-              )
-              .pop();
-
-            if (!assigned && wildCards.length > 0) {
-              console.log("can't assign wildcard");
-              break;
-            }
-
-            if (assigned) {
-              wildCardAssignments[assigned.id] = wildCardAssignments[
-                assigned.id
-              ]
-                ? wildCardAssignments[assigned.id].concat(currentCubeId)
-                : [currentCubeId];
-            }
-          }
-
-          // if pod is STILL not full, fill it with randoms
-          while (preferredPlayers.length < 8) {
-            console.log("have to resort to randoms, uh oh");
-            preferredPlayers.push(
-              ...unassignedPlayers
-                .filter(
-                  (player) =>
-                    !preferredPlayers.find((pp) => pp.id === player.id)
-                )
-                .slice(0, 8 - preferredPlayers.length)
-            );
-          }
-
-          assignments[draftIndex][podNumber - 1] = preferredPlayers;
-
-          console.log(
-            `Draft ${draftIndex + 1}, pod ${podNumber} (cube ${currentCubeId})`,
-            JSON.stringify(
-              preferredPlayers.map(
-                (pp) =>
-                  `${pp.firstName} ${pp.lastName} (${
-                    preferencesByPlayer[pp.id]?.find(
-                      (p) => p.cube === currentCubeId
-                    )?.points ?? "W"
-                  })`
-              )
-            )
-          );
-
-          // clear assigned players from the unassigned list for this draft
-          unassignedPlayers = unassignedPlayers.filter(
-            (player) => !preferredPlayers.find((pp) => pp.id === player.id)
-          );
-
-          // and take this cube away from their preferences
-          preferredPlayers.forEach((pp) => {
-            if (preferencesByPlayer[pp.id]) {
-              const pref = preferencesByPlayer[pp.id].find(
-                (pref) => pref.cube === currentCubeId
-              );
-              if (pref) pref.used = true;
-            } else {
-              wildCardAssignments[pp.id] = wildCardAssignments[pp.id]
-                ? wildCardAssignments[pp.id].concat(currentCubeId)
-                : [currentCubeId];
-            }
-          });
-
-          draftPods.push(
-            await this.appDataSource.getRepository(DraftPod).save({
-              podNumber,
-              draft,
-              cube: cubesByPreference[cubeIndex],
-            })
-          );
-        }
-        await this.generateDraftSeatings(
-          draftPods,
-          assignments[draftIndex].flat()
-        );
-
-        console.log("Preference points used", preferencePointsUsed);
-
-        totalPreferencePointsUsed += preferencePointsUsed;
-        draftIndex++;
-      }
-      console.log("Total preference points used", totalPreferencePointsUsed);
     }
 
     return await this.getTournamentAndDrafts(tournamentId);
@@ -655,5 +477,298 @@ export class TournamentService {
       tournamentId,
       userId
     );
+  }
+
+  async getPreferentialPodAssignments(tournamentId: number) {
+    const tournament = await this.getTournamentAndDrafts(tournamentId);
+    const enrollments = (await this.getTournamentEnrollments(tournamentId))
+      .enrollments;
+    const cubes = await this.cubeService.getCubesForTournament(tournamentId);
+    const podsPerDraft = tournament.totalSeats / 8;
+
+    const podAssignments: PreferentialPodAssignments[] = [];
+
+    const preferences =
+      await this.preferenceService.getPreferencesForTournament(tournamentId);
+
+    const iterationsPerStrategy = 10;
+
+    const podGenerationStrategies: DraftPodGenerationStrategy[][] = [
+      ["greedy", "sparing", "sparing"],
+      ["greedy", "greedy", "sparing"],
+      ["greedy", "greedy", "greedy"],
+    ];
+
+    for (const strategy of podGenerationStrategies) {
+      for (let iteration = 0; iteration < iterationsPerStrategy; ++iteration) {
+        // SET UP FOR A PARTICULAR ITERATION
+        const preferencesByPlayer: PreferencesByPlayer = {};
+        preferences.forEach((preference) => {
+          let currentPlayerPreference =
+            preferencesByPlayer[preference.player.id];
+          const pref = {
+            player: preference.playerId,
+            cube: preference.cube.id,
+            points: preference.points,
+            used: false,
+          };
+
+          if (!currentPlayerPreference) {
+            preferencesByPlayer[preference.player.id] = [pref];
+          } else {
+            currentPlayerPreference.push(pref);
+          }
+        });
+
+        const wildCardAssignments: {
+          [key: number]: number[];
+        } = {};
+
+        let totalPreferencePointsUsed = 0;
+
+        const assignments: User[][][] = makeArray(
+          tournament.drafts.length,
+          podsPerDraft,
+          8
+        );
+
+        let currentIterationAssignments: PreferentialPodAssignments = {
+          preferencePoints: 0,
+          assignments: [],
+        };
+
+        let draftIndex = 0;
+        for (let draft of tournament.drafts.sort(
+          (a, b) => a.draftNumber - b.draftNumber
+        )) {
+          let preferencePointsUsed = 0;
+          let unassignedPlayers = enrollments.map((enroll) => enroll.player);
+          const draftPods: DraftPod[] = [];
+
+          const wildCards = unassignedPlayers
+            .filter(
+              (player) =>
+                !preferencesByPlayer[player.id] ||
+                preferencesByPlayer[player.id].filter((pref) => !pref.used)
+                  .length === 0
+            )
+            .sort(randomize);
+
+          for (let podNumber = 1; podNumber <= podsPerDraft; ++podNumber) {
+            const cubeIndex =
+              strategy[draftIndex] === "greedy"
+                ? 0 // for greedy strategy, take the most popular cube available
+                : podsPerDraft - podNumber; // for sparing strategy, take the Nth most popular where N is pods to be generated
+
+            const cubesByPreference = cubes
+              // filter out cubes already used in this draft
+              .filter(
+                (cube) => !draftPods.find((pod) => pod.cube.id === cube.id)
+              )
+              .map((cube) => ({
+                id: cube.id,
+                points: preferences
+                  .filter(
+                    (pref) =>
+                      pref.cube.id === cube.id &&
+                      !preferencesByPlayer[pref.player.id].find(
+                        (x) => x.cube === cube.id
+                      )?.used
+                  )
+                  .reduce((acc, cur) => acc + cur.points, 0),
+              }))
+              .sort((a, b) => b.points - a.points);
+
+            const currentCubeId = cubesByPreference[cubeIndex].id;
+            const preferredPlayers = preferences // find players who..
+              .filter((pref) => pref.cube.id === currentCubeId) // want to play this cube
+              .sort(randomize)
+              .sort((a, b) => b.points - a.points) // have rated it highly
+              .filter(
+                // have not already been assigned to play it in an earlier draft
+                (pref) =>
+                  !preferencesByPlayer[pref.player.id].find(
+                    (x) => x.cube === currentCubeId
+                  )?.used
+              )
+              .filter(
+                (
+                  pref // and have not been assigned to a different cube in this draft
+                ) =>
+                  unassignedPlayers.find(
+                    (player) => player.id === pref.player.id
+                  )
+              )
+              .slice(0, 8)
+              .map((pref) => {
+                preferencePointsUsed += pref.points;
+                return pref;
+              })
+              .map((pref) => pref.player);
+
+            // if pod is not full, fill it with wildcards
+            while (preferredPlayers.length < 8 && wildCards.length > 0) {
+              // check that the wildcard hasn't played this cube earlier
+              const assigned = wildCards
+                .filter(
+                  (wc) =>
+                    !(wildCardAssignments[wc.id] ?? []).includes(currentCubeId)
+                )
+                .pop();
+
+              if (!assigned && wildCards.length > 0) {
+                // console.log("can't assign wildcard");
+                break;
+              }
+
+              if (assigned) {
+                wildCardAssignments[assigned.id] = wildCardAssignments[
+                  assigned.id
+                ]
+                  ? wildCardAssignments[assigned.id].concat(currentCubeId)
+                  : [currentCubeId];
+              }
+            }
+
+            // if pod is STILL not full, fill it with randoms
+            while (preferredPlayers.length < 8) {
+              preferredPlayers.push(
+                ...unassignedPlayers
+                  .filter(
+                    (player) =>
+                      !preferredPlayers.find((pp) => pp.id === player.id)
+                  )
+                  // TODO remove players who already played this cube
+                  .slice(0, 8 - preferredPlayers.length)
+              );
+            }
+
+            assignments[draftIndex][podNumber - 1] = preferredPlayers;
+
+            /* 
+            console.log(
+              `Draft ${
+                draftIndex + 1
+              }, pod ${podNumber} (cube ${currentCubeId})`,
+              JSON.stringify(
+                preferredPlayers.map(
+                  (pp) =>
+                    `${pp.firstName} ${pp.lastName} (${
+                      preferencesByPlayer[pp.id]?.find(
+                        (p) => p.cube === currentCubeId
+                      )?.points ?? "W"
+                    })`
+                )
+              )
+            );
+            */
+
+            // clear assigned players from the unassigned list for this draft
+            unassignedPlayers = unassignedPlayers.filter(
+              (player) => !preferredPlayers.find((pp) => pp.id === player.id)
+            );
+
+            // and take this cube away from their preferences
+            preferredPlayers.forEach((pp) => {
+              if (preferencesByPlayer[pp.id]) {
+                const pref = preferencesByPlayer[pp.id].find(
+                  (pref) => pref.cube === currentCubeId
+                );
+                if (pref) pref.used = true;
+              } else {
+                wildCardAssignments[pp.id] = wildCardAssignments[pp.id]
+                  ? wildCardAssignments[pp.id].concat(currentCubeId)
+                  : [currentCubeId];
+              }
+            });
+
+            const draftPod = await this.appDataSource
+              .getRepository(DraftPod)
+              .create({
+                podNumber,
+                draft,
+                cube: cubesByPreference[cubeIndex],
+              });
+
+            draftPods.push(draftPod);
+          }
+
+          /* add nothing to db in this method
+          await this.tournamentService.generateDraftSeatings(
+            draftPods,
+            assignments[draftIndex].flat()
+          );
+          */
+
+          console.log("Preference points used", preferencePointsUsed);
+
+          currentIterationAssignments.preferencePoints += preferencePointsUsed;
+          currentIterationAssignments.assignments.push({
+            draftNumber: draft.draftNumber,
+            pods: draftPods.map((pod) => ({
+              cube: pod.cube,
+              players: assignments[draftIndex][pod.podNumber - 1],
+            })),
+          });
+
+          totalPreferencePointsUsed += preferencePointsUsed;
+          draftIndex++;
+        }
+        podAssignments.push(currentIterationAssignments);
+        console.log("Total preference points used", totalPreferencePointsUsed);
+      }
+    }
+
+    const sortedAssignments = podAssignments.sort(
+      (a, b) => b.preferencePoints - a.preferencePoints
+    );
+
+    console.log(
+      "points used in each iteration",
+      sortedAssignments.map((assignment) => assignment.preferencePoints)
+    );
+
+    const preferencesByPlayer: PreferencesByPlayer = {};
+    preferences.forEach((preference) => {
+      let currentPlayerPreference = preferencesByPlayer[preference.player.id];
+      const pref = {
+        player: preference.playerId,
+        cube: preference.cube.id,
+        points: preference.points,
+        used: false,
+      };
+
+      if (!currentPlayerPreference) {
+        preferencesByPlayer[preference.player.id] = [pref];
+      } else {
+        currentPlayerPreference.push(pref);
+      }
+    });
+
+    console.log("best assignment:");
+    sortedAssignments[0].assignments.forEach((assignment) => {
+      console.log("DRAFT", assignment.draftNumber);
+      assignment.pods.forEach((pod, index) => {
+        console.log(
+          "POD",
+          index + 1,
+          "cube",
+          pod.cube.id,
+          pod.players
+            .map(
+              (player) =>
+                player.firstName +
+                " " +
+                player.lastName +
+                " (" +
+                (preferencesByPlayer[player.id]?.find(
+                  (pref) => pref.cube == pod.cube.id
+                )?.points ?? "W") +
+                ")"
+            )
+            .join(", ")
+        );
+      });
+    });
   }
 }
